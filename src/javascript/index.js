b588aa23b8a7f8fe04355e962337bdc5630c80aa
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2024-09-16 12:27:33 -0300

    término dos ajustes de manipular dados na dom com API

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,8 +3,9 @@
 const apiKey = "8411249319fd6709c68cf18072ca9350"
 
 
-const cityInput = document.getElementById("#city_name")
+const cityInput = document.getElementById("city_name")
 const searchButton = document.getElementById("go-search")
+const weatherDescription = document.querySelector("#temp-description")
 const cityElement = document.querySelector("#nome-cidade")
 const tempElement = document.querySelector("#temp-value")
 const temp_maxElement = document.querySelector("#temp-max")
@@ -32,10 +33,17 @@ const showWeatherData = async (city) => {
     
 
     cityElement.innerText = data.name
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+
+    //logica para deixar a primeira letra da palavra em maiúscula
+     //seleciona o inicio e deixa em maiusculo, e o restante em minúsculo
+    weatherDescription.innerText = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1).toLowerCase()
+
     tempElement.innerHTML = `${parseInt(data.main.temp)} <sup>°C<sup/>`
     temp_maxElement.innerHTML = `${parseInt(data.main.temp_max)} <sup>°C<sup/>`
     temp_minElement.innerHTML = `${parseInt(data.main.temp_min)} <sup>°C<sup/>`
-    humidityElement.innerText = data.main.humidity
+    humidityElement.innerHTML = `${data.main.humidity}%`
+    whindElement.innerHTML = `${data.wind.speed} Km/h`
 
 }
 
